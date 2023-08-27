f3c95f1de8c180aa625952c46c420c2bb538cfa0
tudastuk <129007673+tudastuk@users.noreply.github.com>
2023-08-28 02:15:22 +0300

    1

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,15 +4,16 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
-    static URL = "/account";
-    /**
-     * Получает информацию о счёте
-     * */
-    static get(id = "", callback) {
-        createRequest({
-            url: this.URL + "/" + id, // почему лучше это - чем шаблонка? тк визуально шаблон читается в данной ситуации сложнее...
-            method: "GET",
-            callback,
-        });
-    }
+  /**
+   * Получает информацию о счёте
+   * */
+  static URL = '/account';
+
+  static get(id = '', callback){
+    createRequest({
+      url: `${this.URL}/${id}`,
+      method: 'GET',
+      callback: callback
+    })
+  }
 }
