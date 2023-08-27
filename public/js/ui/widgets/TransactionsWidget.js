55a9fd06222a501a025de9b4d7be30d4acafd1cc
tudastuk <129007673+tudastuk@users.noreply.github.com>
2023-08-28 02:22:36 +0300

    8

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -11,8 +11,9 @@ class TransactionsWidget {
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor( element ) {
-
+  constructor(element) {
+    this.element = element;
+    this.registerEvents();
   }
   /**
    * Регистрирует обработчики нажатия на
@@ -21,6 +22,18 @@ class TransactionsWidget {
    * экземпляра окна
    * */
   registerEvents() {
+    this.element
+      .querySelector('.create-income-button')
+      .addEventListener('click', (e) => {
+        e.preventDefault();
+        App.getModal('newIncome').open();
+      });
 
+    this.element
+      .querySelector('.create-expense-button')
+      .addEventListener('click', (e) => {
+        e.preventDefault();
+        App.getModal('newExpense').open();
+      });
   }
 }
