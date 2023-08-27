f62fa397cca3f3f35743e6fb3897de76850199b3
tudastuk <129007673+tudastuk@users.noreply.github.com>
2023-08-28 02:10:54 +0300

    3

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -3,26 +3,35 @@
  * Имеет свойство URL, равно пустой строке.
  * */
 class Entity {
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static list(data, callback){
+    static URL = "";
 
-  }
+    static list(data, callback) {
+        createRequest({
+            url: this.URL,
+            data,
+            method: "GET",
+            callback,
+        });
+    }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
-  static create(data, callback) {
+    static create(data, callback) {
+        createRequest({
+            url: this.URL,
+            data,
+            method: "PUT",
+            callback,
+        });
+    }
 
-  }
-
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
+    static remove(data, callback) {
+        createRequest({
+            url: this.URL,
+            data,
+            method: "DELETE",
+            callback,
+        });
+    }
+}
    * (в зависимости от того, что наследуется от Entity)
    * */
   static remove(data, callback ) {
